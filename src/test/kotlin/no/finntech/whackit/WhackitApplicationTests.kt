654cfd2bf1d80332c29e99fdf35d803a2f62efee
frerodla <fmr@finn.no>
2019-10-17 16:12:57 +0200

    first commit

---
+++ src/test/kotlin/no/finntech/whackit/WhackitApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.finntech.whackit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhackitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
