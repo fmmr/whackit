d962b03b2f0eff40c4edaafa495745c9d43358ed
frerodla <fmr@finn.no>
2019-10-18 09:53:54 +0200

    Added input-handling (for one button)

--- src/main/kotlin/no/finntech/whackit/WhackitApplication.kt
+++ src/main/kotlin/no/finntech/whackit/WhackitApplication.kt
@@ -3,6 +3,11 @@ package no.finntech.whackit
 import com.pi4j.io.gpio.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import com.pi4j.util.CommandArgumentParser.getPin
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent
+import com.pi4j.io.gpio.event.GpioPinListenerDigital
+import com.pi4j.io.gpio.trigger.GpioTrigger
+
 
 @SpringBootApplication
 class WhackitApplication
@@ -10,7 +15,16 @@ class WhackitApplication
 fun main(args: Array<String>) {
 	GpioFactory.setDefaultProvider(RaspiGpioProvider(RaspiPinNumberingScheme.DEFAULT_PIN_NUMBERING))
 	val  gpio = GpioFactory.getInstance()
-	val myLED = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_29, "MyLED", PinState.LOW);
+	val myLED = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "MyLED", PinState.LOW);
 	myLED.blink(300)
+
+
+	val input = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, PinPullResistance.PULL_DOWN)
+	input.addListener(GpioPinListenerDigital { event ->
+		// display pin state on console
+		println(" --> GPIO PIN STATE CHANGE: " + event.pin + " = " + event.state)
+	})
+
+
 	runApplication<WhackitApplication>(*args)
 }
