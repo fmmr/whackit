7a4034fb25a7c324788846621eadbbed7fe49978
frerodla <fmr@finn.no>
2019-10-18 13:46:58 +0200

    Endless loop variant (no spring boot app is started)

--- src/main/kotlin/no/finntech/whackit/WhackitApplication.kt
+++ src/main/kotlin/no/finntech/whackit/WhackitApplication.kt
@@ -15,16 +15,52 @@ val gpio = GpioFactory.getInstance()
 val LOG = LoggerFactory.getLogger("MAIN");
 val BUTTON_ARRAY = listOf(GPIO_28, GPIO_23, GPIO_24, GPIO_25)
 
-val pin28 = gpio.provisionDigitalMultipurposePin(GPIO_28, "BUTTON_28", PinMode.DIGITAL_OUTPUT)
-val pin23 = gpio.provisionDigitalMultipurposePin(GPIO_23, "BUTTON_23", PinMode.DIGITAL_OUTPUT)
-val pin24 = gpio.provisionDigitalMultipurposePin(GPIO_24, "BUTTON_24", PinMode.DIGITAL_INPUT, PinPullResistance.PULL_UP)
-val pin25 = gpio.provisionDigitalMultipurposePin(GPIO_25, "BUTTON_25", PinMode.DIGITAL_INPUT, PinPullResistance.PULL_UP)
+val pin28 = gpio.provisionDigitalOutputPin(GPIO_28, "BUTTON_28", PinState.HIGH)
+val pin23 = gpio.provisionDigitalOutputPin(GPIO_23, "BUTTON_23", PinState.HIGH)
+val pin24 = gpio.provisionDigitalInputPin(GPIO_24, "BUTTON_24", PinPullResistance.PULL_UP)
+val pin25 = gpio.provisionDigitalInputPin(GPIO_25, "BUTTON_25", PinPullResistance.PULL_UP)
 
 fun main(args: Array<String>) {
     GpioFactory.setDefaultProvider(RaspiGpioProvider(RaspiPinNumberingScheme.DEFAULT_PIN_NUMBERING))
     setupLedAndSingleButton()
-    initButtons()
-    runApplication<WhackitApplication>(*args)
+
+
+
+    while (true) {
+        pin28.state = PinState.LOW
+        if (pin24.state.isHigh) {
+            LOG.info("BUTTON PRESSED UPPER LEFT")
+            while(pin24.state.isHigh){
+                // do nothing
+            }
+        }
+        if (pin25.state.isHigh) {
+            LOG.info("BUTTON PRESSED LOWER LEFT")
+            while(pin25.state.isHigh){
+                // do nothing
+            }
+        }
+        pin28.state = PinState.HIGH
+
+        pin23.state = PinState.LOW
+        if (pin24.state.isHigh) {
+            LOG.info("BUTTON PRESSED UPPER RIGHT")
+            while(pin24.state.isHigh){
+                // do nothing
+            }
+        }
+        if (pin25.state.isHigh) {
+            LOG.info("BUTTON PRESSED LOWER RIGHT")
+            while(pin25.state.isHigh){
+                // do nothing
+            }
+        }
+        pin23.state = PinState.HIGH
+    }
+
+
+//    initButtons()
+//    runApplication<WhackitApplication>(*args)
 }
 
 private fun setupLedAndSingleButton() {
@@ -37,18 +73,6 @@ private fun setupLedAndSingleButton() {
     })
 }
 
-fun findButton() {
-    listOf(pin24, pin25).forEach { it.removeAllListeners() }
-
-    val state24 = pin24.state.isHigh
-    val state25 = pin25.state.isHigh
-    setReadState()
-    val state28 = pin28.isHigh
-    val state23 = pin23.isHigh
-    val button = getButton(state28, state23, state24, state25)
-    LOG.info("  YOU PRESSED:  $button")
-    initButtons()
-}
 
 fun getButton(state28: Boolean, state23: Boolean, state24: Boolean, state25: Boolean): String {
     return when {
@@ -63,41 +87,3 @@ fun getButton(state28: Boolean, state23: Boolean, state24: Boolean, state25: Boo
     }
 }
 
-private fun initButtons() {
-    pin28.mode = PinMode.DIGITAL_OUTPUT
-    pin23.mode = PinMode.DIGITAL_OUTPUT
-    pin28.state = PinState.HIGH
-    pin23.state = PinState.HIGH
-
-    pin24.mode = PinMode.DIGITAL_INPUT
-    pin25.mode = PinMode.DIGITAL_INPUT
-    pin24.pullResistance = PinPullResistance.PULL_UP
-    pin25.pullResistance = PinPullResistance.PULL_UP
-
-    listOf(pin24, pin25).forEach {
-        it.addListener(GpioPinListenerDigital { event ->
-            // display pin state on console
-            LOG.trace(" DETECTED ARRAY PRESS  " + event.pin + " = " + event.state)
-            if (event.state == PinState.HIGH) {
-                try {
-                    findButton()
-                } catch (e: Exception) {
-                    LOG.error("Got exception trying to find button: ", e)
-                }
-            }
-        })
-    }
-}
-
-private fun setReadState() {
-    pin24.mode = PinMode.DIGITAL_OUTPUT
-    pin25.mode = PinMode.DIGITAL_OUTPUT
-    pin24.state = PinState.HIGH
-    pin25.state = PinState.HIGH
-
-    pin28.mode = PinMode.DIGITAL_INPUT
-    pin23.mode = PinMode.DIGITAL_INPUT
-    pin23.pullResistance = PinPullResistance.PULL_UP
-    pin28.pullResistance = PinPullResistance.PULL_UP
-
-}
